79b7d1846b867d08bab692d1e6f0c0a158189ddc
btoone <braden@toonetown.com>
2024-02-26 14:56:25 -0700

    Added abstracted repository

---
+++ Models/ITaskRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace mission8_4_6_v2.Models
+{
+    public interface ITaskRepository
+    {
+        List<Task> Tasks { get; }
+        public void AddTask(Task task);
+        public void UpdateTask(Task task);
+        public void DeleteTask(Task task);
+        public Task GetTask(int TaskId);
+    }
+}
---
+++ Models/Repository.cs
@@ -0,0 +1,32 @@
+﻿namespace mission8_4_6_v2.Models
+{
+    public class Repository : ITaskRepository
+    {
+        public TaskContext _context { get; set; }
+        public Repository(TaskContext context) 
+        {
+            _context = context;
+        }
+        public List<Task> Tasks => _context.Tasks.ToList();
+        public void AddTask(Task task)
+        {
+            _context.Tasks.Add(task);
+            _context.SaveChanges();
+        }
+        public void UpdateTask(Task task)
+        {
+            _context.Tasks.Update(task);
+            _context.SaveChanges();
+        }
+        public void DeleteTask(Task task)
+        {
+            _context.Tasks.Remove(task);
+            _context.SaveChanges();
+        }
+        public Task GetTask(int TaskId)
+        {
+            Task task = _context.Tasks.Single(t => t.TaskId == TaskId);
+            return task;
+        }
+    }
+}
