9550393243e038ff42d39891a633c81cd04736dd
btoone <braden@toonetown.com>
2024-02-26 14:42:29 -0700

    Added Models

---
+++ Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace mission8_4_6_v2.Models
+{
+    public class Category
+    {
+        [Key]
+        [Required(ErrorMessage = "CategoryId is a required field")]
+        public int CategoryId { get; set; }
+        [Required(ErrorMessage = "CategoryName is a required field")]
+        public string CategoryName { get; set; } = string.Empty;
+    }
+}
