bb6182f461eade32dcd93994214edec2dc32754c
b-zapata <150874080+b-zapata@users.noreply.github.com>
2024-02-26 13:39:20 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mission8_4_6_v2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
