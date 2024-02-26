b7f0fe5320d37f5780162e3f2ee17e9cd180c973
btoone <braden@toonetown.com>
2024-02-26 14:56:12 -0700

    Added task context file

---
+++ Models/TaskContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace mission8_4_6_v2.Models
+{
+    public class TaskContext : DbContext
+    {
+        public TaskContext(DbContextOptions<TaskContext> options) : base(options) { }
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
