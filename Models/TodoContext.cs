11987b7a217f5629fe7f6524962e313c7fa58c8e
btoone <braden@toonetown.com>
2024-02-26 17:30:16 -0700

    Renamed Task to Todo

---
+++ Models/TodoContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace mission8_4_6_v2.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options) { }
+        public DbSet<Todo> Todos { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
