5560d25301b01307362850a2cb191f230d26ace2
b-zapata <150874080+b-zapata@users.noreply.github.com>
2024-03-01 09:29:01 -0700

    Bare bones controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,15 +13,61 @@ namespace mission8_4_6_v2.Controllers
             _logger = logger;
         }
 
+        [HttpGet]
+        public IActionResult CreateTask()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateTask()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult EditTask()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult EditTask()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult DeleteTask()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DeleteTask()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Tasks()
+        {
+            return View();
+        }
+
+
+        /*
         public IActionResult Index()
         {
             return View();
         }
 
+
         public IActionResult Privacy()
         {
             return View();
         }
+        */
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
